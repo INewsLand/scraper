42ab29597c9841e78f19d76bf9e721a0537db7c5
Andrés García <jose.andres.gm29@gmail.com>
2020-02-03 16:08:19 -0600

    refactor universal

--- app/scraper/spiders/universal.py
+++ app/scraper/spiders/universal.py
@@ -4,29 +4,23 @@ from utils.text import remove_blank_lines
 
 class ElUniversalSpider(scrapy.Spider):
     name = 'ElUniversal'
-
-    def start_requests(self):
-        base_url = 'https://www.eluniversal.com.mx/minuto-x-minuto?page={0}'
-        urls = list()
-        max_pages = 8
-        for page_number in range(0, max_pages):
-            urls.append(base_url.format(page_number))
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'https://www.eluniversal.com.mx/minuto-x-minuto'
+    ]
 
     def parse(self, response):
         news_content_selector = 'div.view-content>div'
-        title_selector = 'h2>a::text'
-        link_selector = 'h2>a::attr(href)'
-        tag_selector = 'h2>span>a::text'
+        title_selector = 'h2 > a::text'
+        link_selector = 'h2 > a::attr(href)'
+        tag_selector = 'h2 > span > a::text'
         for element in response.css(news_content_selector):
-            hour = element.css('::text').get()
-            title = element.css(title_selector).get()
-            link = element.css(link_selector).get()
-            tag = element.css(tag_selector).get()
             yield {
-                'hour': remove_blank_lines(hour),
-                'title': title,
-                'link': link,
-                'tag': tag
+                'hour': remove_blank_lines(element.css('::text').get()),
+                'title': element.css(title_selector).get(),
+                'link': element.css(link_selector).get(),
+                'tag': element.css(tag_selector).get()
             }
+
+        next_page_selector = 'li.pager-next > a'
+        for page in response.css(next_page_selector):
+            yield response.follow(page, callback=self.parse)
