6c0bccb3dedd72f987c07cbd198e9a6640173fee
Andrés García <jose.andres.gm29@gmail.com>
2020-02-06 16:44:35 -0600

    add function to parse latin1 to utf-8

--- app/utils/text.py
+++ app/utils/text.py
@@ -1,4 +1,9 @@
+import unicodedata
+
 def remove_blank_lines(text):
     text = text.replace('\n', '')
     text = text.replace('\r', '')
     return text
+
+def unidecode_data(text):
+    return unicodedata.normalize('NFKD', text)
